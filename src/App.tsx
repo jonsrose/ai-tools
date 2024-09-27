43d2b9b25e222d55f8282bbab259b1c2b5e505d2
Heisenberg <jon@jonrose.com>
2024-09-26 17:33:29 -0700

    layout fixes

--- src/App.tsx
+++ src/App.tsx
@@ -20,22 +20,20 @@ const App: React.FC = () => {
   const closeHelpModal = () => setIsHelpModalOpen(false);
 
   return (
-    <div className="App">
-      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12 relative">
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-          <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-            {/* Common header */}
-            <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">Speech To Text</h1>
-            
-            <Router>
-              <Routes>
-                {routes.map((route) => (
-                  <Route key={route.path} {...(route as RouteProps)} />
-                ))}
-              </Routes>
-            </Router>
-          </div>
+    <div className="App flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-6 relative">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative bg-white shadow-lg sm:rounded-3xl p-10">
+          {/* Common header */}
+          <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">Speech To Text</h1>
+          
+          <Router>
+            <Routes>
+              {routes.map((route) => (
+                <Route key={route.path} {...(route as RouteProps)} />
+              ))}
+            </Routes>
+          </Router>
         </div>
         {/* Help button */}
         <button
